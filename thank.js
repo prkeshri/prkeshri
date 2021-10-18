593ab365bcdd77378b63df761571d4fc731bb707
Praveen <prkeshri@gmail.com>
2021-10-18 19:18:10 +0530

    Files

--- thank.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-const https = require('https');
-https.get('https://raw.githubusercontent.com/prkeshri/prkeshri/master/me',
-    (res)=>{
-        let data = '';
-        res.on('data',_=>data+=_);
-        res.on('end', _=> console.log(data));
-    }
-)
